ac6c18e2c245facb02e81aa1b6f7d96ca981e698
Rock <84492239+RockSolid1106@users.noreply.github.com>
2021-07-19 16:08:28 +0530

    modified:   main.py

--- main.py
+++ main.py
@@ -11,8 +11,6 @@ from keep_alive import keep_alive
 
 keep_alive()
 #This is a change from VSCode to test the integration with GH.
-#I have made these changes from Repl.it.
-#This a change that I have made from VSC.
 #another line from vsc.
 print("This is the PRODUCTION version.")
 
