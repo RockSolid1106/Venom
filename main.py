dab33607272588f1814cb33e741e99e62f9e5333
Rock <84492239+RockSolid1106@users.noreply.github.com>
2021-07-19 06:23:51 +0000

    something who cares

--- main.py
+++ main.py
@@ -8,8 +8,6 @@ from tabulate import tabulate
 from replit import db
 import os
 from keep_alive import keep_alive
-#hello
-#this is a comment because I am changing the code with VSCode with the help of GH.
 keep_alive()
 
 print("This is the PRODUCTION version.")
