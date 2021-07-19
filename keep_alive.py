749259b3b06ed20c5b6a15f0d5823ae1b2995aa5
Rock <84492239+RockSolid1106@users.noreply.github.com>
2021-07-19 17:58:34 +0530

    modified:   keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -1,3 +1,13 @@
+#Copyright © 2021  RockSolid1106
+#This program is free software: you can redistribute it and/or modify
+#it under the terms of the GNU General Public License as published by
+#the Free Software Foundation, either version 3 of the License.
+#
+#This program is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#GNU General Public License for more details.
+
 from flask import Flask
 from threading import Thread
 
